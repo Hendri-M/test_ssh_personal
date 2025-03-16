6c9e50bea6e5a6bc7bac4c37c858f318bdc1efef
Hendri-M <hendrimuji31@gmail.com>
2025-03-17 06:02:27 +0700

    test second push after config ssh

--- heeloo.dart
+++ heeloo.dart
@@ -1,7 +1,15 @@
 void main(List<String> args) {
-  heloo('John');
+  final divide = divideNumb(90, 3);
+  final greeting = heloo('John');
+
+  print(greeting);
+  print(divide);
 }
 
 String heloo(String name) {
   return 'Hello there, $name';
+}
+
+double divideNumb(int a, int b) {
+  return a / b;
 }
